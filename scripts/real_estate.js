ae9badada32fa9dc5143d6b8dd2f69c858105d9e
Chinenye Dominic Eloji <elojichinenye24@gmail.com>
2025-09-10 21:47:14 +0100

    designed navbar

--- scripts/real_estate.js
+++ scripts/real_estate.js
@@ -3,3 +3,8 @@ const header = document.querySelector(".header");
 window.addEventListener("scroll", () => {
   header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
 });
+
+// Opening and closing the navbar
+const navbar = document.querySelector(".navbar");
+const navToggler = document.querySelector(".nav-toggle-btn");
+navToggler.addEventListener("click", () => navbar.classList.toggle('active'));
