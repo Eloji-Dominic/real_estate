2e68957050d95f456cc24a52956a1dc88bed5ab5
Chinenye Dominic Eloji <elojichinenye24@gmail.com>
2025-09-12 21:36:44 +0100

    Building the feature section and adding responsive styling

--- scripts/real_estate.js
+++ scripts/real_estate.js
@@ -1,7 +1,7 @@
 // opening and closing the head scrollbar using javascript code on windows
 const header = document.querySelector(".header");
 window.addEventListener("scroll", () => {
-  header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
+  header.classList[window.scrollY > 100 ? "add" : "remove"]("active");
 });
 
 // Opening and closing the navbar
